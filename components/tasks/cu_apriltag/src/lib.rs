a24c7635e6738c799374bd3a844599b1bf49a190
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2026-01-23 13:38:49 -0700

    fix apriltag config

--- components/tasks/cu_apriltag/src/lib.rs
+++ components/tasks/cu_apriltag/src/lib.rs
@@ -11,11 +11,11 @@ use apriltag_sys::image_u8_t;
 
 use bincode::de::Decoder;
 use bincode::error::DecodeError;
-use cu29::bincode::{Decode, Encode};
-use cu29::prelude::*;
 use cu_sensor_payloads::CuImage;
 use cu_spatial_payloads::EncodableIsometry;
 use cu_spatial_payloads::Pose as CuPose;
+use cu29::bincode::{Decode, Encode};
+use cu29::prelude::*;
 use serde::ser::SerializeTuple;
 use serde::{Deserialize, Deserializer, Serialize};
 
@@ -308,7 +308,7 @@ impl CuTask for AprilTags {
                 cy,
                 tagsize,
             };
-            let camera_id = config.get("camera_id").expect("provide camera id");
+            let camera_id = config.get("camera_id")?.expect("provide camera id");
 
             return Ok(Self {
                 tag_params,
@@ -378,8 +378,8 @@ mod tests {
     use super::*;
     use anyhow::Context;
     use anyhow::Result;
-    use image::{imageops::crop, imageops::resize, imageops::FilterType, Luma};
     use image::{ImageBuffer, ImageReader};
+    use image::{Luma, imageops::FilterType, imageops::crop, imageops::resize};
 
     #[cfg(feature = "hardware")]
     use cu_sensor_payloads::CuImageBufferFormat;
